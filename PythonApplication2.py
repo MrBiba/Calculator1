9b88eb2f9bdff7089e821b79d9357c73d4d0d128
User <User@DESKTOP-QEC5GC4>
2023-09-12 21:59:17 +0600

    Добавьте файлы проекта.

---
+++ PythonApplication2.py
@@ -0,0 +1,15 @@
+# coding=windows-1251
+num1 = float(input("Введи первое число: "))
+num2 = float(input("Введи второе число: " ))
+operator = (input("Введи действие: "))
+
+if operator == "+":
+ print(num1 + num2)
+elif operator == "_":
+ print(num1 - num2)
+elif operator == "/":
+ print(num1 / num2)
+elif operator == "*":
+ print(num1 * num2)
+else:
+ print("Неверный оператор ввода!")
